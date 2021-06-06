f23d2816d2402bf76eea97d85660979912e6bc42
Gabriel Souza <gabrielsouzase@gmail.com>
2021-06-05 21:36:09 -0300

    Projeto CRUD

---
+++ jgcrud-me/src/test/java/com/jgabriel/crud/JgcrudMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgabriel.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgcrudMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
