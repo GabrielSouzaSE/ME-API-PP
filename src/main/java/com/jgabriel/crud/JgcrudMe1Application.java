5898bd8fd63b7e2068f2bf9a1eea9244bf1a1a7b
Gabriel Souza <gabrielsouzase@gmail.com>
2021-06-08 00:31:01 -0300

    Start Project ME-PP

---
+++ src/main/java/com/jgabriel/crud/JgcrudMe1Application.java
@@ -0,0 +1,13 @@
+package com.jgabriel.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgcrudMe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgcrudMe1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgabriel/crud/JgcrudMe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgabriel.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgcrudMe1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
